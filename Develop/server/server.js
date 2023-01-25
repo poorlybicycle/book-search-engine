8004eb0d22a1e6a17a48f4e86acf5c10f3d2b9e6
Mime Jr <petravidovic8@gmail.com>
2023-01-25 13:27:40 -0500

    add apollo server and middleware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,11 +3,19 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//Import ApolloServer Class
+const { ApolloServer } = require('apollo-server-express');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Create new instance of apollo server w GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });};
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +27,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+//call async func to start server
+startApolloServer(typeDefs, resolvers);
